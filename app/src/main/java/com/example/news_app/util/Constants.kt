5e60decfd6ab1e29b91a5810fe0f4dc2f61eb9be
abdul-kioum-ahmed-sumon <abdulkioumahmed@gmail.com>
2024-10-27 21:39:58 +0600

    Fix the News API and add RetrofitInstance

---
+++ app/src/main/java/com/example/news_app/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.news_app.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "568b0c6fd81048e9ab17bd8b3272121d"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY ="500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
